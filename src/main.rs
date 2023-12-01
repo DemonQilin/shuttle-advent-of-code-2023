1d135ba27ad7dbe71b0ae45f1ea09ea64e387550
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-01 08:28:50 -0500

    challenge 1: add support for negative numbers in sled id endpoint

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,26 @@ async fn fake_error() -> StatusCode {
 }
 
 async fn sled_id(Path(packed_ids): Path<String>) -> Result<String, StatusCode> {
+    let mut packed_ids = packed_ids;
+
+    if packed_ids.ends_with('/') {
+        packed_ids = packed_ids[..packed_ids.len() - 1].to_string();
+    }
+
+    let packed_ids = packed_ids;
+
     if packed_ids.is_empty() {
         return Err(StatusCode::BAD_REQUEST);
     }
 
-    let mut id_sled: u32 = 0;
+    let mut id_sled: i32 = 0;
 
     for (index, packed_id) in packed_ids.split('/').enumerate() {
         if index > 19 {
             return Err(StatusCode::BAD_REQUEST);
         }
 
-        let id: u32 = packed_id.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
+        let id: i32 = packed_id.parse().map_err(|_| StatusCode::BAD_REQUEST)?;
         id_sled ^= id;
     }
 
