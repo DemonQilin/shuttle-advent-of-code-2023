ce61b009c3c92bbd2d61a5a02be90e1c92c9b22d
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-04 07:15:07 -0500

    feat(Challenge 4): Add serde dependency and implement endpoint to calculate total strength of reindeers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
-use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use axum::{
+    extract::{Json, Path},
+    http::StatusCode,
+    routing::{get, post},
+    Router,
+};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Reindeer {
+    name: String,
+    strength: i32,
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -37,12 +49,21 @@ async fn sled_id(Path(packed_ids): Path<String>) -> Result<String, StatusCode> {
     Ok(id_sled.to_string())
 }
 
+async fn get_total_strength(Json(reindeers): Json<Vec<Reindeer>>) -> String {
+    reindeers
+        .into_iter()
+        .map(|reindeer| reindeer.strength)
+        .sum::<i32>()
+        .to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/-1/error", get(fake_error))
-        .route("/1/*packed_ids", get(sled_id));
+        .route("/1/*packed_ids", get(sled_id))
+        .route("/4/strength", post(get_total_strength));
 
     Ok(router.into())
 }
