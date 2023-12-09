a574765a9703557ab8fde73b61e2bddce5b36a64
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-08 21:22:17 -0500

    feat(Bonus 6): Counts "elf on a shelf" and "shelf" with no elf

--- src/hidden_elves.rs
+++ src/hidden_elves.rs
@@ -1,9 +1,21 @@
 use axum::{routing::post, Router};
 
 async fn count_elf_in_input(body: String) -> String {
-    let total = body.split("elf").count() - 1;
+    let body = body.to_lowercase();
 
-    total.to_string()
+    let total_elf = body.split("elf").count() - 1;
+    let total_elf_in_shelf = body.split("elf on a shelf").count() - 1;
+    let total_shelf_with_no_elf = body
+        .split("elf on a shelf")
+        .collect::<String>()
+        .split("shelf")
+        .count()
+        - 1;
+
+    format!(
+        "{{\"elf\":{},\"elf on a shelf\": {},\"shelf with no elf on it\":{}}}",
+        total_elf, total_elf_in_shelf, total_shelf_with_no_elf
+    )
 }
 
 pub fn get_hidden_elves_routes() -> Router {
