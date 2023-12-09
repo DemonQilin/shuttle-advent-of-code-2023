434043325473b439f8e3ee3692ed5bbd75fb7010
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-09 14:49:04 -0500

    feat(Bonus 7): Create Ingredients, Recipe and Recipe structs

--- src/santa_cookies.rs
+++ src/santa_cookies.rs
@@ -1,11 +1,86 @@
-use std::collections::HashMap;
-
 use axum::{
     http::{header::COOKIE, HeaderMap, HeaderValue},
     routing::get,
     Router,
 };
 use base64::prelude::*;
+use serde::{Deserialize, Serialize};
+
+use std::{
+    collections::HashMap,
+    ops::{Mul, Sub},
+};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Ingredients {
+    flour: u32,
+    sugar: u32,
+    butter: u32,
+    baking_powder: u32,
+    chocolate_chips: u32,
+}
+
+#[derive(Debug, Serialize)]
+struct RecipeResult {
+    cookies: u32,
+    pantry: Ingredients,
+}
+
+#[derive(Debug, Deserialize)]
+struct Recipe {
+    recipe: Ingredients,
+    pantry: Ingredients,
+}
+
+impl Sub for Ingredients {
+    type Output = Ingredients;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            flour: self.flour - rhs.flour,
+            sugar: self.sugar - rhs.sugar,
+            butter: self.butter - rhs.butter,
+            baking_powder: self.baking_powder - rhs.baking_powder,
+            chocolate_chips: self.chocolate_chips - rhs.chocolate_chips,
+        }
+    }
+}
+
+impl Mul<u32> for Ingredients {
+    type Output = Self;
+
+    fn mul(self, rhs: u32) -> Self::Output {
+        Self {
+            flour: self.flour * rhs,
+            sugar: self.sugar * rhs,
+            butter: self.butter * rhs,
+            baking_powder: self.baking_powder * rhs,
+            chocolate_chips: self.chocolate_chips * rhs,
+        }
+    }
+}
+
+impl Recipe {
+    fn bake(self) -> RecipeResult {
+        let total_cookies = *[
+            self.pantry.flour / self.recipe.flour,
+            self.pantry.sugar / self.recipe.sugar,
+            self.pantry.butter / self.recipe.butter,
+            self.pantry.baking_powder / self.recipe.baking_powder,
+            self.pantry.chocolate_chips / self.recipe.chocolate_chips,
+        ]
+        .iter()
+        .min()
+        .unwrap();
+
+        let remain = self.pantry - (self.recipe * total_cookies);
+
+        RecipeResult {
+            cookies: total_cookies,
+            pantry: remain,
+        }
+    }
+}
 
 fn get_cookies_map(headers: &HeaderMap) -> Result<HashMap<String, HeaderValue>, String> {
     let cookies = headers.get_all(COOKIE);
