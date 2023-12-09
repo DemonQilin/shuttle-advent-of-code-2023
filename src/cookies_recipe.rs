87767f644b253bf59d4a9ddb79d2679ccd63d3b9
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-09 09:52:21 -0500

    feat(Challenge 7 - Solution): Add route to decode the recipe cookie

--- src/cookies_recipe.rs
+++ src/cookies_recipe.rs
@@ -1,5 +1,18 @@
-use axum::Router;
+use axum::{http::HeaderMap, routing::get, Router};
+use base64::prelude::*;
+
+async fn get_encoded_cookies_recipe(headers: HeaderMap) -> Result<String, String> {
+    let cookie = headers.get("cookie").ok_or("A cookie is expected")?;
+    let recipe = cookie.as_bytes().splitn(2, |&v| v == b'=').last().unwrap();
+
+    let decode_recipe = BASE64_STANDARD
+        .decode(recipe)
+        .map_err(|_| "The recipe cookie is not valid")?;
+    let decode_recipe = String::from_utf8(decode_recipe).unwrap();
+
+    Ok(decode_recipe)
+}
 
 pub fn get_cookies_recipe_routes() -> Router {
-    Router::new()
+    Router::new().route("/decode", get(get_encoded_cookies_recipe))
 }
