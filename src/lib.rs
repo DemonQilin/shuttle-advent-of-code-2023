721c504f43627dcdf21d926ff9f5c66b0eb89a58
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-09 20:50:34 -0500

    feat(Common): Refactor module imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-pub mod hidden_elves;
+mod hidden_elves;
 mod reindeer;
-pub mod santa_cookies;
+mod santa_cookies;
 mod sled;
 
+pub use hidden_elves::get_hidden_elves_routes;
 pub use reindeer::get_reindeer_routes;
+pub use santa_cookies::get_cookies_recipe_routes;
 pub use sled::get_sled_routes;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use axum::{http::StatusCode, routing::get, Router};
-use cch23_demonqilin01::{get_reindeer_routes, get_sled_routes, hidden_elves, santa_cookies};
+use cch23_demonqilin01::{
+    get_cookies_recipe_routes, get_hidden_elves_routes, get_reindeer_routes, get_sled_routes,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -16,8 +18,8 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/-1/error", get(fake_error))
         .nest("/1", get_sled_routes())
         .nest("/4", get_reindeer_routes())
-        .nest("/6", hidden_elves::get_hidden_elves_routes())
-        .nest("/7", santa_cookies::get_cookies_recipe_routes());
+        .nest("/6", get_hidden_elves_routes())
+        .nest("/7", get_cookies_recipe_routes());
 
     Ok(router.into())
 }
