51158827ae6f78a0a5b9c4990270c96f78067fbd
Demonqilin <velezloaizaesteban9012015@gmail.com>
2023-12-09 22:32:14 -0500

    feat(Challenge 8): Create weight route and its handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod hidden_elves;
+mod pokemon;
 mod reindeer;
 mod santa_cookies;
 mod sled;
 
 pub use hidden_elves::get_hidden_elves_routes;
+pub use pokemon::get_pokemon_routes;
 pub use reindeer::get_reindeer_routes;
 pub use santa_cookies::get_cookies_recipe_routes;
 pub use sled::get_sled_routes;
---
+++ src/pokemon.rs
@@ -0,0 +1,45 @@
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use serde::Deserialize;
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize)]
+struct Pokemon {
+    id: u16,
+    name: String,
+    weight: Hectogram,
+}
+
+#[derive(Debug, Deserialize)]
+struct Hectogram(u16);
+struct Kilogram(u16);
+
+impl Hectogram {
+    fn to_kilogram(&self) -> Kilogram {
+        Kilogram(self.0 / 10)
+    }
+}
+
+async fn get_pokemon_weight(Path(pokemon_id): Path<u16>) -> (StatusCode, String) {
+    let url = format!("https://pokeapi.co/api/v2/pokemon/{pokemon_id}");
+    let res = reqwest::get(url).await;
+    if let Err(e) = res {
+        if e.is_request() {
+            let msg = format!("The request for pokemon {pokemon_id} failed");
+            return (StatusCode::NOT_FOUND, msg);
+        }
+
+        return (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            "Something went wrong".into(),
+        );
+    }
+
+    let pokemon = res.unwrap().json::<Pokemon>().await.unwrap();
+    let kilo_weight = pokemon.weight.to_kilogram().0;
+
+    (StatusCode::OK, kilo_weight.to_string())
+}
+
+pub fn get_pokemon_routes() -> Router {
+    Router::new().route("/weight/:pokemon_id", get(get_pokemon_weight))
+}
